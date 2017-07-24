7f48efed513f6030f692c011f9679da4ce03e9a4
vbn <zhoucan123@vip.qq.com>
2017-07-24 19:14:08 +0800

    create product file

--- build.sh
+++ build.sh
@@ -8,12 +8,17 @@ UNIVERSAL_OUTPUTFOLDER=$dir/universal
 rm -rf $simBuildDir
 rm -rf $osBuildDir
 rm -rf $UNIVERSAL_OUTPUTFOLDER
+rm -rf $UNIVERSAL_OUTPUTFOLDER/Libs
 
 mkdir $simBuildDir
 mkdir $osBuildDir
 mkdir $UNIVERSAL_OUTPUTFOLDER
+mkdir $UNIVERSAL_OUTPUTFOLDER/Libs
 
 xcodebuild -scheme $PROJECT_NAME -sdk iphonesimulator -configuration "release" -derivedDataPath $simBuildDir
 xcodebuild -scheme $PROJECT_NAME -sdk iphoneos -configuration "release" -derivedDataPath $osBuildDir
 
 lipo -create -output "$UNIVERSAL_OUTPUTFOLDER/$PROJECT_NAME.framework" "$simBuildDir/Build/Products/Release-iphonesimulator/$PROJECT_NAME.framework/$PROJECT_NAME" "$osBuildDir/Build/Products/Release-iphoneos/$PROJECT_NAME.framework/$PROJECT_NAME"
+
+#复制第三方文件
+cp -R $dir/KNProjectFramework/KNVenders/ $UNIVERSAL_OUTPUTFOLDER/Libs
